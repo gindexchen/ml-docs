e89235fc08400d027aaf03e1ec5e2bcfd7967158
gindexchen <gindexchen@gmail.com>
2018-06-12 10:13:09 +0800

    Add files via upload

---
+++ mnist_siamese.py
@@ -0,0 +1,150 @@
+rom __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+
+import random
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Input, Flatten, Dense, Dropout, Lambda
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import RMSprop
+from keras import backend as K
+from keras.layers.merge import concatenate
+from sklearn.metrics import classification_report
+
+num_classes = 10
+epochs = 20
+
+
+def euclidean_distance(vects):
+    x, y = vects
+    return K.sqrt(K.maximum(K.sum(K.square(x - y), axis=1, keepdims=True), K.epsilon()))
+
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
+
+
+def contrastive_loss(y_true, y_pred):
+    '''Contrastive loss from Hadsell-et-al.'06
+    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    '''
+    margin = 1
+    return K.mean(y_true * K.square(y_pred) +
+                  (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
+
+
+def create_pairs(x, digit_indices):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
+    '''
+    pairs = []
+    labels = []
+    n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
+    for d in range(num_classes):
+        for i in range(n):
+            z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
+            pairs += [[x[z1], x[z2]]]
+            inc = random.randrange(1, num_classes)
+            dn = (d + inc) % num_classes
+            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
+            pairs += [[x[z1], x[z2]]]
+            labels += [1, 0]
+    return np.array(pairs), np.array(labels)
+
+
+def create_base_network(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+    x = Flatten()(input)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    
+    #x = Conv2D(32, kernel_size=(3, 3),activation='relu')(input)
+    #x = Conv2D(64, (3, 3), activation='relu')(x)
+    #x = MaxPooling2D(pool_size=(2, 2))(x)
+    #x = Dropout(0.25)(x)
+    #x = Flatten()(x)
+    #x = Dense(128, activation='relu')(x)
+    
+    return Model(input, x)
+
+
+def compute_accuracy(y_true, y_pred):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    pred = y_pred.ravel() < 0.5
+    return np.mean(pred == y_true)
+    
+    #print(classification_report(y_true,y_pred))
+
+
+def accuracy(y_true, y_pred):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
+
+
+# the data, shuffled and split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+img_rows, img_cols = 28, 28
+#x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+#x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = x_train.shape[1:]
+
+# create training+test positive and negative pairs
+digit_indices = [np.where(y_train == i)[0] for i in range(num_classes)]
+tr_pairs, tr_y = create_pairs(x_train, digit_indices)
+
+digit_indices = [np.where(y_test == i)[0] for i in range(num_classes)]
+te_pairs, te_y = create_pairs(x_test, digit_indices)
+
+# network definition
+base_network = create_base_network(input_shape)
+
+input_a = Input(shape=input_shape)
+input_b = Input(shape=input_shape)
+
+# because we re-use the same instance `base_network`,
+# the weights of the network
+# will be shared across the two branches
+processed_a = base_network(input_a)
+processed_b = base_network(input_b)
+
+# 不用欧式距离
+#distance = Lambda(euclidean_distance,
+#                 output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+
+# 将配对的特征水平合并为一个特征
+merge = concatenate([processed_a,processed_b])
+# 增加一个隐藏层
+out = Dense(128,activation='relu',kernel_regularizer=regularizers.l2(0.01))(merge)
+# 二分类比较
+out = Dense(1,activation='sigmoid')(out)
+
+model = Model([input_a, input_b], out)
+
+# train
+rms = RMSprop()
+# 不使用自定义的损失函数，优化器也改用rmsprop， 度量改为'accuracy'
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+history = model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y, batch_size=128,epochs=epochs,validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y))
+
+# compute final accuracy on training and test sets
+y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
+# 计算训练集上精度，阈值取0.5
+y_pred = np.where(y_pred<0.5,0,1).ravel()
+print("train accuracy:",np.mean(tr_y==y_pred))
+# 计算测试集上精度
+y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
+y_pred = np.where(y_pred<0.5,0,1).ravel()
+print("test accuracy:",np.mean(te_y==y_pred))
